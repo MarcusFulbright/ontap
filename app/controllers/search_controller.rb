4a7fa045feedabb55998456d00559634e58239c5
Marcus Fulbright <fulbright.marcus@gmail.com>
2012-11-13 10:50:53 -0600

    should have been in last commit

--- app/controllers/search_controller.rb
+++
@@ -1,7 +0,0 @@
-class SearchController < ApplicationController
-require 'pintlabs_api'
-  def create
-    params[:search][:query] #=> this is your query string. pass it along to lib/whateverAPI
-  end
-
-end
